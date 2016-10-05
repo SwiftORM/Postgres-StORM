a0a457e2980c2a89f6f5de4c114fe02ccc12b80e
Jonathan Guthrie <jono@perfect.org>
2016-10-05 10:32:40 -0400

    fixing case

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/SwiftORM/STORM.git", majorVersion: 0, minor: 0),
+		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
 	],
 	exclude: []
 )
