b51c90e4299289792ce294896ef2dffe7cbe0d83
Jonathan Guthrie <jono@perfect.org>
2016-10-05 10:32:28 -0400

    adding files

---
+++ Sources/Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Convenience.swift
+//  PostgresSTORM
+//
+//  Created by Jonathan Guthrie on 2016-10-04.
+//
+//
+
+import Foundation
---
+++ Sources/PostgresStORM.swift
@@ -0,0 +1,13 @@
+//
+//  PostgreStORM.swift
+//  PostgresSTORM
+//
+//  Created by Jonathan Guthrie on 2016-10-03.
+//
+//
+
+import StORM
+
+open class PostgresStORM: StORM {
+
+}
