80360c76ee5595aa367f8c24e3fe9ebc3b10486b
Jonathan Guthrie <jono@perfect.org>
2016-10-05 10:34:28 -0400

    refactor to new open class

--- Sources/PostgresConnect.swift
+++ Sources/PostgresConnect.swift
@@ -10,11 +10,11 @@ import Foundation
 import StORM
 import PostgreSQL
 
-class PostgresConnect: Connect {
+open class PostgresConnect: StORMConnect {
 	// server connection
 	
-	private let server = PGConnection()
-	public var rows = StORMResultSet()
+	public let server = PGConnection()
+	//public var rows = StORMResultSet()
 
 
 	/// Init with no credentials
@@ -33,7 +33,7 @@ class PostgresConnect: Connect {
 		super.init()
 		self.database = database
 		self.datasource = .Postgres
-		self.credentials = DataSourceCredentials(host: host, port: port, user: username, pass: password)
+		self.credentials = StORMDataSourceCredentials(host: host, port: port, user: username, pass: password)
 	}
 
 	// Connection String
@@ -42,7 +42,7 @@ class PostgresConnect: Connect {
 	}
 
 	// Initiates the connection
-	private func connect() {
+	public func open() {
 		let status = server.connectdb(self.connectionString())
 		if status != .ok {
 			print(status)
@@ -52,66 +52,8 @@ class PostgresConnect: Connect {
 		}
 	}
 
-	// Internal function which executes statements, with parameter binding
-	// Returns raw result
-	func exec(_ statement: String, params: [String]) -> PGResult {
-		self.connect()
-		defer { server.close() }
-		self.statement = statement
-		return server.exec(statement: statement, params: params)
-	}
-
-	// Internal function which executes statements, with parameter binding
-	// Returns a processed row set
-	func execRows(_ statement: String, params: [String]) -> [StORMRow] {
-		self.connect()
-		defer { server.close() }
-		self.statement = statement
-		let result = server.exec(statement: statement, params: params )
-		let resultRows = parseRows(result)
-		result.clear()
-		return resultRows
-	}
 
 
 }
 
 
-extension PostgresConnect {
-	fileprivate func parseRows(_ result: PGResult) -> [StORMRow] {
-		var resultRows = [StORMRow]()
-
-		let num = result.numTuples()
-
-		for x in 0..<num { // rows
-			var vals = [String: Any]()
-
-			for f in 0..<result.numFields() {
-
-				switch PostgresMap(Int(result.fieldType(index: f)!)) {
-				case "Int":
-					vals[result.fieldName(index: f)!] = result.getFieldInt(tupleIndex: x, fieldIndex: f)
-				case "Bool":
-					vals[result.fieldName(index: f)!] = result.getFieldBool(tupleIndex: x, fieldIndex: f)
-				case "String":
-					vals[result.fieldName(index: f)!] = result.getFieldString(tupleIndex: x, fieldIndex: f)
-					// json
-					// xml
-					// float
-					// date
-					// time
-					// timestamp
-					// timestampz
-				// jsonb
-				default:
-					vals[result.fieldName(index: f)!] = result.getFieldString(tupleIndex: x, fieldIndex: f)
-				}
-
-			}
-			let thisRow = StORMRow()
-			thisRow.data = vals
-			resultRows.append(thisRow)
-		}
-		return resultRows
-	}
-}
--- Sources/PostgresStORM.swift
+++ Sources/PostgresStORM.swift
@@ -7,7 +7,76 @@
 //
 
 import StORM
+import PostgreSQL
 
 open class PostgresStORM: StORM {
+	open var connection = PostgresConnect()
 
+	// table
+	open var table = ""
+	// table structure
+	//open var cols = [String: String]()
+
+	// Internal function which executes statements, with parameter binding
+	// Returns raw result
+	func exec(_ statement: String, params: [String]) -> PGResult {
+		connection.open()
+		defer { connection.server.close() }
+		connection.statement = statement
+		return connection.server.exec(statement: statement, params: params)
+	}
+
+	// Internal function which executes statements, with parameter binding
+	// Returns a processed row set
+	func execRows(_ statement: String, params: [String]) -> [StORMRow] {
+		connection.open()
+		defer { connection.server.close() }
+		connection.statement = statement
+		let result = connection.server.exec(statement: statement, params: params )
+		let resultRows = parseRows(result)
+		result.clear()
+		return resultRows
+	}
+
+
+}
+
+
+extension PostgresStORM {
+	fileprivate func parseRows(_ result: PGResult) -> [StORMRow] {
+		var resultRows = [StORMRow]()
+
+		let num = result.numTuples()
+
+		for x in 0..<num { // rows
+			var vals = [String: Any]()
+
+			for f in 0..<result.numFields() {
+
+				switch PostgresMap(Int(result.fieldType(index: f)!)) {
+				case "Int":
+					vals[result.fieldName(index: f)!] = result.getFieldInt(tupleIndex: x, fieldIndex: f)
+				case "Bool":
+					vals[result.fieldName(index: f)!] = result.getFieldBool(tupleIndex: x, fieldIndex: f)
+				case "String":
+					vals[result.fieldName(index: f)!] = result.getFieldString(tupleIndex: x, fieldIndex: f)
+					// json
+					// xml
+					// float
+					// date
+					// time
+					// timestamp
+					// timestampz
+				// jsonb
+				default:
+					vals[result.fieldName(index: f)!] = result.getFieldString(tupleIndex: x, fieldIndex: f)
+				}
+
+			}
+			let thisRow = StORMRow()
+			thisRow.data = vals
+			resultRows.append(thisRow)
+		}
+		return resultRows
+	}
 }
