25009b9006fb74a0fceb761660110dafa74ca2c6
Jonathan Guthrie <jono@perfect.org>
2016-10-05 15:17:53 -0400

    part of a wider update, errors and discarding

--- Sources/SQL.swift
+++ Sources/SQL.swift
@@ -13,9 +13,14 @@ import PostgreSQL
 extension PostgresStORM {
 
 	/// Execute Raw SQL (with parameter binding)
-	/// Returns PGResult
-	public func sql(_ statement: String, params: [String]) -> PGResult {
-		return exec(statement, params: params)
+	/// Returns PGResult (discardable)
+	@discardableResult
+	public func sql(_ statement: String, params: [String]) throws -> PGResult {
+		do {
+			return try exec(statement, params: params)
+		} catch {
+			throw StORMError.error(error.localizedDescription)
+		}
 	}
 
 
