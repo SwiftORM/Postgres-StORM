3369715990f0f911ab74b7cc7b9d0a6bcdc3764f
Jonathan Guthrie <jono@perfect.org>
2016-10-05 10:33:00 -0400

    adding PostgresStORM convenience methods stub

--- Sources/Convenience.swift
+++ Sources/Convenience.swift
@@ -6,4 +6,29 @@
 //
 //
 
-import Foundation
+
+import PerfectLib
+import StORM
+import PostgreSQL
+
+extension PostgresStORM {
+
+	/// Deletes one row, with an id
+	/// Presumes first property in class is the id.
+	public func delete() {
+		let (idname, idval) = firstAsKey()
+		let _ = exec(deleteSQL(self.table, idName: idname, limit: 1), params: [idval as! String])
+	}
+
+	public func save() {
+
+	}
+
+	public func create() {
+		// postgres equivalent of last insert id?
+	}
+
+	public func find() {
+		
+	}
+}
