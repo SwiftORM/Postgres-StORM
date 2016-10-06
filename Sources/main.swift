0b468ac04a887e4c92b4c0e907bb287ad14949c4
Jonathan Guthrie <jono@perfect.org>
2016-10-06 10:22:57 -0400

    change how table is defined, so move out inits. start move to convenience method testing

--- Sources/main.swift
+++ Sources/main.swift
@@ -24,13 +24,9 @@ class User: PostgresStORM {
 	var lastname		: String = ""
 	var email			: String = ""
 
-	override init() {
-		super.init()
 
-		// initialize which table
-		// keeps it out of the var set which will pollute introspection
-		// some ORM's require tablename to be the same as classname.
-		table = "users"
+	override open func table() -> String {
+		return "users"
 	}
 
 	override func to(_ this: StORMRow) {
@@ -54,59 +50,106 @@ class User: PostgresStORM {
 	}
 }
 
-
-
 print("====================================================")
-print("INSERT: ")
-var obj = User()
-obj.connection = connect
 
-do {
-	obj.id = try obj.insert(cols: ["firstname","lastname","email"], params: ["Donkey", "Kong", "donkey.kong@mailinator.com"]) as! Int
-	print("New ID: \(obj.id)")
+var obj = User(connect)
+//obj.connection = connect    // Use if object was instantiated without connection
+obj.firstname = "X"
+obj.lastname = "Y"
 
-} catch {
-	print("Error detacted: \(error)")
-}
+try obj.save {id in obj.id = id as! Int }
+print(obj.id)
 
 
-print("====================================================")
-print("UPDATE: ")
-obj.firstname = "Mickey"
-obj.lastname = "Mouse"
-obj.email = "Mickey.Mouse@mailinator.com"
-
-do {
-	var updateResponse = try obj.update(cols: ["firstname","lastname","email"], params: [obj.firstname, obj.lastname, obj.email], idName: "id", idValue: obj.id)
-	print("UPDATE RESPONSE: \(updateResponse)")
-} catch {
-	print("Error detacted: \(error)")
-}
-
-print("====================================================")
-print("DELETE: ")
-
-do {
-	//try obj.delete(id: obj.id)
-	try obj.delete()
-} catch {
-	print("Error detacted: \(error)")
-}
+obj.firstname = "A"
+obj.lastname = "B"
+try obj.save()
+print(obj.id)
 
 
-
-print("====================================================")
-print("SELECT: ")
-obj = User()
-obj.connection = connect
-
-do {
-	try obj.select(whereclause: "firstname = $1", params: ["Joe"], orderby: ["id"])
-	print("\(obj.firstname) \(obj.lastname)")
-
-} catch {
-	print("Error detacted: \(error)")
-}
+//print("====================================================")
+//print("INSERT: ")
+//var obj = User()
+//obj.connection = connect
+//
+//do {
+////	obj.id = try obj.insert(cols: ["firstname","lastname","email"], params: ["Donkey", "Kong", "donkey.kong@mailinator.com"]) as! Int
+////	print("New ID: \(obj.id)")
+//
+//	obj.firstname	= "Mister"
+//	obj.lastname	= "PotatoHead"
+//	obj.email		= "potato@example.com"
+//	try obj.create()
+//	print("New ID from create is: \(obj.id)")
+//} catch {
+//	print("Error detacted: \(error)")
+//}
+//
+//
+//print("====================================================")
+//print("UPDATE: ")
+//obj.firstname = "Mickey"
+//obj.lastname = "Mouse"
+//obj.email = "Mickey.Mouse@mailinator.com"
+//
+//do {
+//	var updateResponse = try obj.update(cols: ["firstname","lastname","email"], params: [obj.firstname, obj.lastname, obj.email], idName: "id", idValue: obj.id)
+//	print("UPDATE RESPONSE: \(updateResponse)")
+//} catch {
+//	print("Error detacted: \(error)")
+//}
+//
+//print("====================================================")
+//print("DELETE: ")
+//
+////do {
+////	//try obj.delete(id: obj.id)
+////	try obj.delete()
+////} catch {
+////	print("Error detacted: \(error)")
+////}
+//
+//
+//
+//print("====================================================")
+//print("SELECT: ")
+//obj = User()
+//obj.connection = connect
+//
+//do {
+//	try obj.select(whereclause: "firstname = $1", params: ["Joe"], orderby: ["id"])
+//	print("\(obj.firstname) \(obj.lastname)")
+//
+//} catch {
+//	print("Error detacted: \(error)")
+//}
+//
+//print("====================================================")
+//print("GET (id): ")
+//obj = User()
+//obj.connection = connect
+//
+//do {
+//	try obj.get(2)
+//	print("\(obj.firstname) \(obj.lastname)")
+//
+//} catch {
+//	print("Error detacted: \(error)")
+//}
+//
+//print("====================================================")
+//print("GET (): ")
+//obj = User()
+//obj.connection = connect
+//
+//do {
+//	obj.id = 7
+//	try obj.get()
+//	print("\(obj.firstname) \(obj.lastname)")
+//
+//} catch {
+//	print("Error detacted: \(error)")
+//}
 
 print("====================================================")
 
