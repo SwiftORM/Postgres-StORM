b2e6171a5c65a1b370822d2c960cd9a06c6ba4b7
Jonathan Guthrie <jono@perfect.org>
2016-10-06 10:20:02 -0400

    change to using table func

--- Sources/Convenience.swift
+++ Sources/Convenience.swift
@@ -13,26 +13,23 @@ import PostgreSQL
 
 extension PostgresStORM {
 
-	public func create() {
-		// postgres equivalent of last insert id? ->>>> RETURNING.
-		
-	}
+	// create is in main PostgresStORM now.
 
 	/// Deletes one row, with an id
 	/// Presumes first property in class is the id.
 	public func delete() throws {
 		let (idname, idval) = firstAsKey()
 		do {
-			try exec(deleteSQL(self.table, idName: idname), params: [String(describing: idval)])
+			try exec(deleteSQL(self.table(), idName: idname), params: [String(describing: idval)])
 		} catch {
 			throw StORMError.error(error.localizedDescription)
 		}
 	}
 
 	public func get(_ id: Any) throws {
-		let (idname, _) = firstAsKey() // ignore idval...
+		let (idname, _) = firstAsKey()
 		do {
-			try select(whereclause: "\(idname) = ", params: [id as! String], orderby: [])
+			try select(whereclause: "\(idname) = $1", params: [String(describing: id)], orderby: [])
 		} catch {
 			throw StORMError.error(error.localizedDescription)
 		}
@@ -41,15 +38,12 @@ extension PostgresStORM {
 	public func get() throws {
 		let (idname, idval) = firstAsKey()
 		do {
-			try select(whereclause: "\(idname) = ", params: [idval as! String], orderby: [])
+			try select(whereclause: "\(idname) = $1", params: [String(describing: idval)], orderby: [])
 		} catch {
 			throw StORMError.error(error.localizedDescription)
 		}
 	}
 
-	public func save() {
-
-	}
 
 	public func find() {
 		
