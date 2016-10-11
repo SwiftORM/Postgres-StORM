46bb88c45b982ccc1f58754dd0b3f3160a090ccd
Jonathan Guthrie <jono@perfect.org>
2016-10-11 14:42:52 -0400

    debug

--- Tests/PostgresSTORMTests/PostgresSTORMTests.swift
+++ Tests/PostgresSTORMTests/PostgresSTORMTests.swift
@@ -67,6 +67,7 @@ class PostgresSTORMTests: XCTestCase {
 	Save - New
 	============================================================================================= */
 	func testSaveNew() {
+		print(connect.host)
 		let obj = User(connect)
 		//obj.connection = connect    // Use if object was instantiated without connection
 		obj.firstname = "X"
