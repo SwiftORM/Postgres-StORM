1796569568e2fa94ea04c6141261c2a62bd881ad
Jonathan Guthrie <jono@perfect.org>
2016-10-17 10:31:56 -0400

    consistent package name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "PostgresSTORM",
+	name: "PostgresStORM",
 	targets: [],
 	dependencies: [
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
