89b119f73189702303ec8597be60f44854a67bcd
Jonathan Guthrie <jono@perfect.org>
2016-12-07 21:37:36 -0500

    consistent format

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
 		.Package(url:"https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0),
-		Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
 	],
 	exclude: []
 )
