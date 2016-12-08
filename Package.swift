73aa1be8e49129b4929f05b48f161cfa4a9d38c4
Jonathan Guthrie <jono@perfect.org>
2016-12-08 12:26:29 -0500

    Update to ORM 1.0.x; linux exclusions

--- Package.swift
+++ Package.swift
@@ -1,21 +1,13 @@
-//
-//  Package.swift
-//  Postgres StORM
-//
-//  Created by Jonathan Guthrie on 2016-09-23.
-//	Copyright (C) 2016 Jonathan Guthrie.
-//
-
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-12-08 17:14:38 +0000
 import PackageDescription
-
 let package = Package(
-	name: "PostgresStORM",
-	targets: [],
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 0, minor: 0),
-		.Package(url:"https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
-	],
-	exclude: []
+    name: "PostgresStORM",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+    ]
 )
