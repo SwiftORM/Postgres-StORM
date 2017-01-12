c77750ef5a157abb43b5af97578443a3597d3160
Jonathan Guthrie <jono@perfect.org>
2017-01-12 17:16:14 -0500

    update logger rev

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 17:14:38 +0000
+// Date: 2017-01-12 22:14:50 +0000
 import PackageDescription
 let package = Package(
     name: "PostgresStORM",
@@ -8,6 +8,6 @@ let package = Package(
         .Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1, minor: 0),
     ]
 )
