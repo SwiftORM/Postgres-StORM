9ce7ba051b0d62479db0288792349444772b350e
Jonathan Guthrie <jono@perfect.org>
2017-08-03 17:16:44 -0400

    adding dataconverter helper method for toArrayString

---
+++ Sources/DataConverters.swift
@@ -0,0 +1,24 @@
+//
+//  DataConverters.swift
+//  PostgresStORM
+//
+//  Created by Jonathan Guthrie on 2017-08-03.
+//
+//
+
+import Foundation
+
+extension PostgresStORM {
+
+	fileprivate func trim(_ str: String) -> String {
+		return str.trimmingCharacters(in: .whitespacesAndNewlines)
+	}
+
+	public func toArrayString(_ input: Any) -> [String] {
+		return (input as? String ?? "").characters.split(separator: ",").map{ trim(String($0)) }
+	}
+	public func toArrayInt(_ input: Any) -> [Int] {
+		// Needs refinement. Should not be 0
+		return (input as? String ?? "").characters.split(separator: ",").map{ Int(trim(String($0))) ?? 0 }
+	}
+}
