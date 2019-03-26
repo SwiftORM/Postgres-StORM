0f20b71a70c65af81bcd77c0cc00ae0b4f19f10f
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-03-26 13:14:41 -0400

    Updated package file

--- Package.swift
+++ Package.swift
@@ -1,13 +1,18 @@
+// swift-tools-version:4.2
 // Generated automatically by Perfect Assistant Application
 // Date: 2018-02-23 18:59:18 +0000
 import PackageDescription
 let package = Package(
 	name: "PostgresStORM",
-	targets: [],
+	products: [
+		.library(name: "PostgresStORM", targets: ["PostgresStORM"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", majorVersion: 3),
-		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 3),
-//		.Package(url: "https://github.com/PerfectlySoft/Perfect-XML.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-PostgreSQL.git", from: "3.0.0"),
+		.package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+	],
+	targets: [
+		.target(name: "PostgresStORM", dependencies: ["PerfectPostgreSQL", "StORM", "PerfectLogger"])
 	]
 )
